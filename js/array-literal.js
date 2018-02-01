42b47d903e16d6c67185b76c614bff3b46d66411
G00333093 <G00333093@gmit.ie>
2018-02-01 16:19:19 +0000

    Add files via upload

---
+++ js/array-literal.js
@@ -0,0 +1,11 @@
+var colors = new Array('white' ,
+					   'black' ,
+					   'red') ;
+					   
+//colors = ['red', 'black', 'white'] ;
+
+colors.push("purple");
+
+
+var el = document.getElementById('colors') ; 
+el.textContent = colors[3];
